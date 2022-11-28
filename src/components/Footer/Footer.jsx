8128850a0f695b208b0a5134731c194eb0659ca3
konivan <konivan08@gmail.com>
2022-11-28 17:53:46 +0300

    add: adaptive footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,43 +11,66 @@ export const Footer = (props) => {
     <footer className={style.footer}>
       <div className={style.wrapper}>
         <div className={style.container}>
-          <div className={style.column}>
-            <img src="img/footer/logo.svg" alt="logo" />
-            <p>Юридический адрес</p>
-          </div>
-          <div className={style.column}>
-            <div className={style.item}>
-              <img src="img/footer/lk.svg" alt="lk" onClick={() => props.setShowAuth(true)}/>
-              <span onClick={() => props.setShowAuth(true)}>Личный кабинет</span>
+          <div className={style.row}>
+            <div className={style.box}>
+              <div className={style.column}>
+                <div className={style.item}>
+                  <span>Закажите у нас проект</span>
+                  <span>уже сегодня!</span>
+                </div>
+                <Button setShowOrders={props.setShowOrders}>Заказать</Button>
+              </div>
+              <div className={style.column}>
+                <img src="img/footer/logo.svg" alt="logo" />
+                <p>Юридический адрес</p>
+              </div>
             </div>
-            <div className={style.item}>
-              {IMG.map((item, index) => (
-                <a href={item.href} key={`${item} ${index}`}>
-                  <img src={item.img} alt={item.alt} />
-                </a>
+          </div>
+          <div className={style.row}>
+            <div className={style.column}>
+              <img src="img/footer/logo.svg" alt="logo" />
+              <p>Юридический адрес</p>
+            </div>
+            <div className={style.column}>
+              <div className={style.item}>
+                <img
+                  src="img/footer/lk.svg"
+                  alt="lk"
+                  onClick={() => props.setShowAuth(true)}
+                />
+                <span onClick={() => props.setShowAuth(true)}>
+                  Личный кабинет
+                </span>
+              </div>
+              <div className={style.item}>
+                {IMG.map((item, index) => (
+                  <a href={item.href} key={`${item} ${index}`}>
+                    <img src={item.img} alt={item.alt} />
+                  </a>
+                ))}
+              </div>
+            </div>
+            <div className={style.column}>
+              {TEXT.map((item, index) => (
+                <ul key={`${item} ${index}`}>
+                  <li>
+                    <a href={item.href}>{item.text}</a>
+                  </li>
+                </ul>
               ))}
             </div>
-          </div>
-          <div className={style.column}>
-            {TEXT.map((item, index) => (
-              <ul key={`${item} ${index}`}>
-                <li>
-                  <a href={item.href}>{item.text}</a>
-                </li>
-              </ul>
-            ))}
-          </div>
-          <div className={style.column}>
-            <div className={style.item}>
-              <span>Закажите у нас проект</span>
-              <span>уже сегодня!</span>
+            <div className={style.column}>
+              <div className={style.item}>
+                <span>Закажите у нас проект</span>
+                <span>уже сегодня!</span>
+              </div>
+              <Button setShowOrders={props.setShowOrders}>Заказать</Button>
             </div>
-            <Button setShowOrders={props.setShowOrders}>Заказать</Button>
-          </div>
-          <div className={style.column}>
-            <a href="/">
-              <img src="img/footer/icon.svg" alt="icon" />
-            </a>
+            <div className={style.column}>
+              <a href="/">
+                <img src="img/footer/icon.svg" alt="icon" />
+              </a>
+            </div>
           </div>
         </div>
       </div>
