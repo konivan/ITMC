d747caa1cacde2257f06f26c97b7f56d624a3c60
konivan <konivan08@gmail.com>
2022-11-29 17:36:21 +0300

    feat: post orders

--- src/components/Forms/Order/order.jsx
+++ src/components/Forms/Order/order.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import style from './order.module.scss';
 
 import { Icon } from '@iconify/react';
@@ -6,43 +6,110 @@ import { Icon } from '@iconify/react';
 
 const Order = (props) => {
 
+    const [name, setName] = useState('')
+    const [telegram, setTelegram] = useState('')
+    const [description, setDescription] = useState('')
+
+    const orderData = {
+        orders: {
+          name_created: name,
+          telephone_number: telegram,
+          telegram: telegram,
+          description: description,
+        },
+      };
+    
+
     if (!props.showOrders) {
         return null
     }
 
+    const url = "http://localhost:8000/account/api/orders/";
+    const reqOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify(orderData),
+    };
+
+    const sendOrder = () => {
+      fetch(url, reqOptions, orderData)
+        .then((res) => res.json())
+        .then(() => {
+            setName('')
+            setTelegram('')
+            setDescription('')
+            props.setShowOrders(false)
+        })
+        .then(() => props.onClose)
+        .catch((err) => console.log("Error: " + err));
+    };
+
     return (
-        <main className={style.modal} onClick={props.onClose}>
-            <div className={style.modalContent} onClick={e => e.stopPropagation()}>
-                <Icon className={style.closeBtn} icon="mingcute:close-fill" color="white" width="20" height="20" inline={true} onClick={props.onClose}/>
-                <header className={style.modalHeader}>
-                    <h2 className={style.modalTitle}>Заказать услугу</h2>
-                </header>
-                <section className={style.orderLogoWrapper}>
-                    <h2>IT разработка</h2>
-                    <img alt="checkbox" src="img/forms/IT.svg"/>
-                </section>
-                <section className={style.modalInputsWrapper}>
-                    <div className={style.modalInputs}>
-                        <div className={style.modalEmailWrapper}>
-                            <span>Ваше имя</span>
-                            <input placeholder="Введите имя"></input>
-                        </div>
-                        <div className={style.modalPasswordWrapper}>
-                            <span>Номер телефона или Telegram</span>
-                            <input type="email" placeholder="Введите ваш номер"></input>
-                        </div>
-                    </div>
-                    <section className={style.modalDescSection}>
-                        <div className={style.modalDescWrapper}>
-                            <span>Описание</span>
-                            <textarea placeholder="Кратко опишите ваш проект"></textarea>
-                        </div>
-                    </section>
-                </section>
-                <button className={style.enterBtn}>Оставить заявку</button>
+      <main className={style.modal} onClick={props.onClose}>
+        <div
+          className={style.modalContent}
+          onClick={(e) => e.stopPropagation()}
+        >
+          <Icon
+            className={style.closeBtn}
+            icon="mingcute:close-fill"
+            color="white"
+            width="20"
+            height="20"
+            inline={true}
+            onClick={props.onClose}
+          />
+          <header className={style.modalHeader}>
+            <h2 className={style.modalTitle}>Заказать услугу</h2>
+          </header>
+          <section className={style.orderLogoWrapper}>
+            <h2>IT разработка</h2>
+            <img alt="checkbox" src="img/forms/IT.svg" />
+          </section>
+          <section className={style.modalInputsWrapper}>
+            <div className={style.modalInputs}>
+              <div className={style.modalEmailWrapper}>
+                <span>Ваше имя</span>
+                <input
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
+                  value={name}
+                  placeholder="Введите имя"
+                ></input>
+              </div>
+              <div className={style.modalPasswordWrapper}>
+                <span>Номер телефона или Telegram</span>
+                <input
+                  onChange={(e) => {
+                    setTelegram(e.target.value)
+                  }}
+                  value={telegram}
+                  type="email"
+                  placeholder="Введите ваш номер"
+                ></input>
+              </div>
             </div>
-        </main>
-    )   
+            <section className={style.modalDescSection}>
+              <div className={style.modalDescWrapper}>
+                <span>Описание</span>
+                <textarea
+                  onChange={(e) => {
+                    setDescription(e.target.value)
+                  }}
+                  value={description}
+                  placeholder="Кратко опишите ваш проект"
+                ></textarea>
+              </div>
+            </section>
+          </section>
+          <button onClick={sendOrder} className={style.enterBtn}>Оставить заявку</button>
+        </div>
+      </main>
+    );   
 }
 
 export default Order;
