116aea5e72408ff8a0ffd01c73f43e819b8ffe5a
Chernikov-Maksim <chernikov-max13@mail.ru>
2022-12-09 13:58:06 +0300

    target

--- src/components/Partnery/Partnery.jsx
+++ src/components/Partnery/Partnery.jsx
@@ -8,13 +8,13 @@ export const Partnery = () => {
         <div className={style.container}>
           <h2>Наши партнеры</h2>
           <div className={style.column}>
-            <a href="http://www.sheksna-shipping.ru/"><img src="img/partnery/sheksna.svg" alt="sheksna" /></a>
-            <a href="https://mtservicesltd.com/"><img src="img/partnery/mts.svg" alt="mts" /></a>
-            <a href="https://masd.games/"><img src="img/partnery/masd.svg" alt="masd" /></a>
+            <a target="_blank" href="http://www.sheksna-shipping.ru/"><img src="img/partnery/sheksna.svg" alt="sheksna" /></a>
+            <a target="_blank" href="https://mtservicesltd.com/"><img src="img/partnery/mts.svg" alt="mts" /></a>
+            <a target="_blank" href="https://masd.games/"><img src="img/partnery/masd.svg" alt="masd" /></a>
           </div>
           <div className={style.column}>
-            <a href="https://www.mexc.com/ru-RU"><img src="img/partnery/mexc.svg" alt="mexc" /></a>
-            <a href="http://killnet73.ru/"><img src="img/partnery/aiLab.svg" alt="aiLab" /></a>
+            <a target="_blank" href="https://www.mexc.com/ru-RU"><img src="img/partnery/mexc.svg" alt="mexc" /></a>
+            <a target="_blank" href="http://killnet73.ru/"><img src="img/partnery/aiLab.svg" alt="aiLab" /></a>
           </div>
         </div>
       </section>
