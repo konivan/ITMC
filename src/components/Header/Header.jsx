0588cdda75a56df2dd7b4583bc15232d30b0ed19
Chernikov-Maksim <chernikov-max13@mail.ru>
2023-01-19 13:34:59 +0300

    router

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,13 @@ import { NavLink } from "react-router-dom";
 
 export const Header = (props) => {
   const [activeBurger, setActiveBurger] = useState(false);
-  if (window.location.pathname === '/Origin' || window.location.pathname === '/LkCreateOrder' || window.location.pathname === '/Orders' || window.location.pathname === '/Accounts' || window.location.pathname === '/Dogovor') {
+  if (
+    window.location.pathname === "/Origin" ||
+    window.location.pathname === "/LkCreateOrder" ||
+    window.location.pathname === "/Orders" ||
+    window.location.pathname === "/Accounts" ||
+    window.location.pathname === "/Dogovor"
+  ) {
     return null;
   }
   return (
@@ -18,48 +24,83 @@ export const Header = (props) => {
       <div className={style.wrapper}>
         <div className={style.container}>
           <div className={style.column}>
-            <NavLink to="/" onClick={() => {
-            setTimeout(() => {
-              window.scroll(0, 0);
-            }, 0);
-            }}>
+            <NavLink
+              to="/"
+              onClick={() => {
+                setTimeout(() => {
+                  window.scroll(0, 0);
+                }, 0);
+              }}
+            >
               <img src="img/header/logoITMC.svg" alt="logo" />
             </NavLink>
             <div className={style.item}>
-            <img onClick={() => props.setShowAuth(true)} style={{display: props.isAuth ? 'none' : null}} className={style.lk} src="img/header/lk.svg" alt="lk" />
-            <div className={activeBurger === true ? style.burgerActive : style.burger} onClick={() => setActiveBurger(!activeBurger)}>
-              <span></span>
-              <div onClick={(e) => e.stopPropagation()}>
-                <Menu setShowAuthOrders={props.setShowAuthOrders} setIsAuth={props.setIsAuth} isAuth={props.isAuth} activeBurger={activeBurger}/>
+              <img
+                onClick={() => props.setShowAuth(true)}
+                style={{ display: props.isAuth ? "none" : null }}
+                className={style.lk}
+                src="img/header/lk.svg"
+                alt="lk"
+              />
+              <div
+                className={
+                  activeBurger === true ? style.burgerActive : style.burger
+                }
+                onClick={() => setActiveBurger(!activeBurger)}
+              >
+                <span></span>
+                <div onClick={(e) => e.stopPropagation()}>
+                  <Menu
+                    setShowAuthOrders={props.setShowAuthOrders}
+                    setIsAuth={props.setIsAuth}
+                    isAuth={props.isAuth}
+                    activeBurger={activeBurger}
+                  />
+                </div>
               </div>
             </div>
-            </div>
-            </div>
-          <List/>
+          </div>
+          <List />
           <div className={style.column}>
             <div className={style.item}>
-              <Button setShowAuthOrders={props.setShowAuthOrders}>Заказать</Button>
+              <Button setShowAuthOrders={props.setShowAuthOrders}>
+                Заказать
+              </Button>
             </div>
             <div className={style.item}>
-              {localStorage.getItem('auth') === 'false' ? <MyButton setShowAuth={props.setShowAuth}>Войти</MyButton> :
-              <div>
-                <img className={style.userIcon} src="img/header/user.svg" alt="user-icon"/>
-                <div className={style.dropdown}>
-                  <NavLink to="/Origin" onClick={() => {
-                      setTimeout(() => {
-                        window.location.reload();
-                      }, 1)
-                    }}>
-                    <span>Личный кабинет</span>
-                  </NavLink>
-                  <span onClick={() => {
-                    localStorage.setItem('auth', 'false');
-                    props.setIsAuth(false);
-                    localStorage.removeItem('name');
-                    localStorage.removeItem('password');
-                  }}>Выйти</span>
+              {localStorage.getItem("auth") === "false" ? (
+                <MyButton setShowAuth={props.setShowAuth}>Войти</MyButton>
+              ) : (
+                <div>
+                  <img
+                    className={style.userIcon}
+                    src="img/header/user.svg"
+                    alt="user-icon"
+                  />
+                  <div className={style.dropdown}>
+                    <NavLink
+                      to="/Origin"
+                      onClick={() => {
+                        setTimeout(() => {
+                          window.location.reload();
+                        }, 1);
+                      }}
+                    >
+                      <span>Личный кабинет</span>
+                    </NavLink>
+                    <span
+                      onClick={() => {
+                        localStorage.setItem("auth", "false");
+                        props.setIsAuth(false);
+                        localStorage.removeItem("name");
+                        localStorage.removeItem("password");
+                      }}
+                    >
+                      Выйти
+                    </span>
+                  </div>
                 </div>
-              </div>}
+              )}
             </div>
           </div>
         </div>
